3d0b48be4eb9583ca37f957ec3d8ed5140f87c76
hareendrasamarasekara <hareendrasamarasekara@gmail.com>
2024-04-22 23:17:56 -0500

    Add new data

--- db.sql
+++ db.sql
@@ -8,9 +8,9 @@ CREATE TABLE IF NOT EXISTS Movie (
     id varchar(255),
     title varchar(255),
     description varchar(1000),
-    image varchar(255),
+    imageUrl varchar(255),
     genre varchar(255),
-    trailer varchar(255),
+    trailerUrl varchar(255),
     active boolean,
     releaseDate varchar(255),
     duration int,
@@ -66,6 +66,9 @@ CREATE TABLE IF NOT EXISTS Ticket (
     FOREIGN KEY (showTime) REFERENCES ShowTime(id)
 );
 
-INSERT INTO Movie VALUES ( '001', 'Civil War', 'In a dystopian future America, a team of military-embedded journalists races against time to reach Washington, D.C., before rebel factions descend upon the White House.', '', 'Action', 'https://www.youtube.com/watch?v=c2G18nIVpNE', true, '04/12/2024', 109);
+INSERT INTO Movie VALUES ( '001', 'Civil War', 'In a dystopian future America, a team of military-embedded journalists races against time to reach Washington, D.C., before rebel factions descend upon the White House.', 'https://www.cinemark.com/media/2myfnthx/lg-new-cw-poster.jpg', 'Action', 'https://www.youtube.com/watch?v=c2G18nIVpNE', true, '04/12/2024', 109);
 INSERT INTO Movie VALUES ( '002', 'Godzilla x Kong: The New Empire', 'The epic battle continues! Legendary Pictures’ cinematic Monsterverse follows up the explosive showdown of “Godzilla vs. Kong” with an all‐new adventure that pits the almighty Kong and the fearsome Godzilla against a colossal undiscovered threat hidden within our world, challenging their very existence—and our own. “Godzilla x Kong: The New Empire” delves further into the histories of these Titans and their origins, as well as the mysteries of Skull Island and beyond, while uncovering the mythic battle that helped forge these extraordinary beings and tied them to humankind forever.',
-    '', 'Action', 'https://www.youtube.com/watch?v=qqrpMRDuPfc', true, '03/29/2024', 115);
+    'https://www.cinemark.com/media/eogd40rl/lg-godzilla-xke-final-poster.jpg', 'Action', 'https://www.youtube.com/watch?v=qqrpMRDuPfc', true, '03/29/2024', 115);
+INSERT INTO Movie VALUES ( '003', 'Abigail', 'Six criminals capture ballet dancer Abigail, the daughter of a powerful underworld figure within New York City, and bring her to a secluded mansion upstate. The group, led by informant Lambert, is told not to disclose any personal information amongst themselves or to Abigail.', 'https://www.cinemark.com/media/aisex4g2/lg-abigail-poster.jpg', 'Horror, Thriller', 'https://www.youtube.com/watch?v=xtAL2x58hns', true, '04/19/2024', 109);
+INSERT INTO Movie VALUES ( '004', 'Ghostbusters: Frozen Empire', 'In Ghostbusters: Frozen Empire, the Spengler family returns to where it all started – the iconic New York City firehouse – to team up with the original Ghostbusters, who’ve developed a top-secret research lab to take busting ghosts to the next level. But when the discovery of an ancient artifact unleashes an army of ghosts that casts a death chill upon the city, Ghostbusters new and old must join forces to protect their home and save the world from a second Ice Age.', 'https://www.cinemark.com/media/ynoi0pe3/lg-gbfe-ft-poster.jpg', 'Adventure, Mystery, Comedy', 'https://www.youtube.com/watch?v=k_6CbpF2FSk', true, '03/22/2024', 115);
+INSERT INTO Movie VALUES ( '005', 'The Ministry of Ungentlemanly Warfare', 'From the director of Sherlock Holmes and the producer of Top Gun: Maverick. A film by Guy Ritchie.', 'https://www.cinemark.com/media/rrcejfqm/lg-mougw-new-poster.jpg', 'Action, Comedy', 'https://www.youtube.com/watch?v=zvwDen1Wrx8', true, '04/19/2024', 120);
